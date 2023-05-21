125196dc36c0a029a49de556d8dd489353d63d54
FourFourteen <foxwill03@gmail.com>
2023-05-21 18:39:40 -0400

    ADD: Deepslate Zircon Ore Endstone Zircon Ore Nether Zircon Ore Zircon Ore Zircon Block

--- src/main/java/net/ro/tutorialmod/item/ModCreativeModeTab.java
+++ src/main/java/net/ro/tutorialmod/item/ModCreativeModeTab.java
@@ -2,12 +2,13 @@ package net.ro.tutorialmod.item;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.ro.tutorialmod.block.ModBlocks;
 
 public class ModCreativeModeTab {
     public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab("tutorialtab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.ZIRCON.get());
+            return new ItemStack(ModBlocks.ZIRCON_BLOCK.get());
         }
     };
 }
